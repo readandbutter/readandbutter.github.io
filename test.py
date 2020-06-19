baac2062749c57510fc17b66d3362c0c7c6ca8ab
readandbutter <67165024+readandbutter@users.noreply.github.com>
2020-06-19 15:09:35 -0700

    Python test

---
+++ test.py
@@ -0,0 +1,16 @@
+print('heck u python')
+
+hayman = ['husban','partner']
+for i in hayman:
+    print('hayman is')
+    print(i)
+
+
+# URL = "https://www.worldometers.info/coronavirus/"
+# r = requests.get(URL)
+# soup = BeautifulSoup(r.content, 'html5lib')
+# countHTML = soup.find('div', attrs = {'class':'content-inner'})
+#
+# for countVar in countHTML.findAll('div', attrs = {'class':'maincounter-number'}):
+#     count = countVar.span
+#
